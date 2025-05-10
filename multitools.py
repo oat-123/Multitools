1450dd65349084f605ba4dbb951a97cb26cd11ab
oat-123 <Teerapat.th5@gmail.com>
2025-05-10 10:51:55 +0700

    Update multitools.py

--- multitools.py
+++ multitools.py
@@ -50,9 +50,9 @@ if mode == "night_duty":
         df = pd.read_csv(csv_url)
         # แสดงเฉพาะคอลัมน์ A–H
         st.components.v1.iframe(
-            "https://docs.google.com/spreadsheets/d/1PjT38W2Zx7KV764yv9Vjwo9i0TJPacRI0iUGzP0ItAU/edit?usp=sharing",
-            height=700, width=1000)
-
+            src="https://docs.google.com/spreadsheets/d/1PjT38W2Zx7KV764yv9Vjwo9i0TJPacRI0iUGzP0ItAU/pubhtml?gid=0&single=true&range=A1:I100&widget=false&headers=false",
+            height=700,
+            width=1000)
         st.success("โหลดข้อมูลเวรยืนกลางคืนสำเร็จ")
 
         # ลิงก์แก้ไข
