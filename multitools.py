67514cea7d7f3aa787ec57e6b5805f3c85d671cc
oat-123 <Teerapat.th5@gmail.com>
2025-05-10 11:05:36 +0700

    Update multitools.py

--- multitools.py
+++ multitools.py
@@ -49,11 +49,14 @@ if mode == "night_duty":
     try:
         df = pd.read_csv(csv_url)
         # แสดงเฉพาะคอลัมน์ A–H
-        iframe_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR8pO9068jsukCJL0guT_dF7I5cjYMMIhsu7ah-1DkPxSMxnYFsSkuRgffvSUJKVZzQccQyJEOPxvvg/pubhtml?gid=0&single=true&range=A1:I100"
-        st.markdown(f"""
-        <iframe src="https://docs.google.com/viewer?embedded=true&url={pdf_url}" width="100%" height="800px"></iframe>
-        """, unsafe_allow_html=True)
-                st.success("โหลดข้อมูลเวรยืนกลางคืนสำเร็จ")
+        iframe_html = """
+        <div style="transform: scale(0.70); transform-origin: top left; width: 133.33%; height: 800px; overflow: hidden;">
+            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR8pO9068jsukCJL0guT_dF7I5cjYMMIhsu7ah-1DkPxSMxnYFsSkuRgffvSUJKVZzQccQyJEOPxvvg/pubhtml?gid=0&single=true&range=A1:I100&widget=true&headers=false"
+                    width="100%" height="100%" style="border: none;"></iframe>
+        </div>
+        """
+        
+        st.markdown(iframe_html, unsafe_allow_html=True)
 
         # ลิงก์แก้ไข
         edit_link = "https://docs.google.com/spreadsheets/d/1PjT38W2Zx7KV764yv9Vjwo9i0TJPacRI0iUGzP0ItAU/edit"
