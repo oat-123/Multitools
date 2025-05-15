6d3d6a42bca4162abd7a780fe80359a35ca17c0c
oat-123 <Teerapat.th5@gmail.com>
2025-05-15 11:12:48 +0700

    Update multitools.py

--- multitools.py
+++ multitools.py
@@ -29,7 +29,8 @@ if st.sidebar.button("เข้าสู่ระบบ"):
         st.session_state["logged_in"] = True
         st.session_state["username"] = username
         st.session_state["sheet_name"] = users[username]["sheet_name"]
-        st.sidebar.success(f"ยินดีต้อนรับ {username}/n ตอนนี้คุณกำลังใช้ฐานข้อมูลจาก : {st.session_state['sheet_name']}")
+        st.sidebar.success(f"ยินดีต้อนรับ {username}/n ")
+        st.sidebar.success(f"ตอนนี้คุณกำลังใช้ฐานข้อมูลจาก : {st.session_state['sheet_name']}")
     else:
         st.sidebar.error("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
 
