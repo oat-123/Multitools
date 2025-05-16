ce10c7edc24a4dd4707d5ba5531da29e74b68a03
oat-123 <Teerapat.th5@gmail.com>
2025-05-16 10:07:43 +0700

    Update multitools.py

--- multitools.py
+++ multitools.py
@@ -258,7 +258,7 @@ if mode == "night_duty":
 
 elif mode == "weekend_duty":
     st.info("คุณเลือก: เวรเสาร์-อาทิตย์")
-    st.markdown("[คลิกเพื่อดู Google Sheet](https://docs.google.com/spreadsheets/d/1ufm0LPa4c903jhlANKn_YqNyMtG9id0iN-tMHrhNRA8/edit?gid=1888956716)", unsafe_allow_html=True)
+    st.markdown("[คลิกเพื่อดู Google Sheet📃](https://docs.google.com/spreadsheets/d/1ufm0LPa4c903jhlANKn_YqNyMtG9id0iN-tMHrhNRA8/edit?gid=1888956716)", unsafe_allow_html=True)
 
 elif mode == "home":
     st.header("📋 พิมพ์ยอดปล่อย")
